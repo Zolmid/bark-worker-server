2637004cd35e6ddf5f9270f6351dd2f47fc60f57
Zolmid <208783544+Zolmid@users.noreply.github.com>
2026-03-09 11:47:57 +0800

    Update icon fallback map with new URLs and names

--- src/core/icon-fallback-map.ts
+++ src/core/icon-fallback-map.ts
@@ -15,19 +15,20 @@ export function normalizeName(s: string): string {
  * when developers update their app icons. Verify and update these entries periodically.
  */
 export const ICON_FALLBACK_MAP: Record<string, string> = {
-  telegram:
-    'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/47/f5/59/47f55928-a47d-f91a-df2f-03fc4b2994a7/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/512x512bb.jpg',
-  'telegram messenger':
-    'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/47/f5/59/47f55928-a47d-f91a-df2f-03fc4b2994a7/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/512x512bb.jpg',
-  wechat:
-    'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/32/87/01/3287015c-0af4-a2fa-bac6-63b8cef70e8f/AppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/512x512bb.jpg',
-  微信: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/32/87/01/3287015c-0af4-a2fa-bac6-63b8cef70e8f/AppIcon-0-0-1x_U007epad-0-1-0-0-85-220.png/512x512bb.jpg',
-  whatsapp:
-    'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/15/5e/da/155eda69-1c32-eb14-e7c5-a6e6efab9b65/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-85-220.png/512x512bb.jpg',
-  gmail:
-    'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/c3/e5/86/c3e58621-43e9-e7fb-5c6b-6c05e9a1ea6e/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-85-220.png/512x512bb.jpg',
-  youtube:
-    'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/6e/e3/15/6ee315ba-d55e-9040-9827-1e5a60a1b025/YouTubeAppIcon-0-0-1x_U007emarketing-0-10-0-85-220.png/512x512bb.jpg',
+  Telegram:
+    'https://static-r2.zolmid.com/Static/APP%20LOGO/Telegram%20Messenger-iOS-512x512.png',
+  QQ:
+    'https://static-r2.zolmid.com/Static/APP%20LOGO/QQ-iOS-512x512.png',
+  闲鱼:
+    'https://static-r2.zolmid.com/Static/APP%20LOGO/闲鱼%20-%20神奇的闲鱼！-iOS-512x512.png',
+  微信: 
+    'https://static-r2.zolmid.com/Static/APP%20LOGO/微信-iOS-512x512.png',
+  WhatsApp:
+    'https://static-r2.zolmid.com/Static/APP%20LOGO/WhatsApp%20Messenger-iOS-512x512.png',
+  信息:
+    'https://static-r2.zolmid.com/Static/APP%20LOGO/信息-iOS-512x512.png',
+  电话:
+    'https://static-r2.zolmid.com/Static/APP%20LOGO/电话-iOS-512x512.png',
 };
 
 /** Look up an icon URL from the fallback map using the given app name. */
