7d1bc05f5913a1fee3c35c81c52699c55b97aa1e
Zolmid <208783544+Zolmid@users.noreply.github.com>
2026-03-09 12:02:06 +0800

    Update bundle identifiers in ICON_MAP

--- src/core/icon-map.ts
+++ src/core/icon-map.ts
@@ -5,19 +5,19 @@
  * This is the sole source for resolving icon URLs from bundle identifiers.
  */
 export const ICON_MAP: Record<string, string> = {
-  'ph.nicegram.Telegram':
+  'org.telegram.messenger':
     'https://static-r2.zolmid.com/Static/APP%20LOGO/Telegram%20Messenger-iOS-512x512.png',
-  'com.tencent.mqq':
+  'com.tencent.mobileqq':
     'https://static-r2.zolmid.com/Static/APP%20LOGO/QQ-iOS-512x512.png',
-  'com.taobao.fleamarket':
+  'com.taobao.idlefish':
     'https://static-r2.zolmid.com/Static/APP%20LOGO/闲鱼%20-%20神奇的闲鱼！-iOS-512x512.png',
-  'com.tencent.xin':
+  'com.tencent.mm':
     'https://static-r2.zolmid.com/Static/APP%20LOGO/微信-iOS-512x512.png',
-  'net.whatsapp.WhatsApp':
+  'com.whatsapp':
     'https://static-r2.zolmid.com/Static/APP%20LOGO/WhatsApp%20Messenger-iOS-512x512.png',
-  'com.apple.MobileSMS':
+  'com.android.mms':
     'https://static-r2.zolmid.com/Static/APP%20LOGO/信息-iOS-512x512.png',
-  'com.apple.mobilephone':
+  'com.android.incallui':
     'https://static-r2.zolmid.com/Static/APP%20LOGO/电话-iOS-512x512.png',
 };
 
